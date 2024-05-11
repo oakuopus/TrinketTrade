8013a31f55b0c2468435763e887c68cc293c6ea2
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-05-10 18:18:08 -0700

    Add files via upload

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -3,4 +3,17 @@ $(function() {
     $("#hamburger").on("click", function(){
         $(".dropped").slideToggle("slow");
     });
+
+    $(window).on("scroll", function(){ // function to create dynamic header, which only shows when scrollTop is not 0
+        if($(window).scrollTop() == 0){
+            $("header, #hamburger").css({
+                "background-color": "initial"
+            });
+        }else{
+        $("header, #hamburger").css({
+            "background-color": "black",
+            "opacity": "90%",
+        });
+        }   
+    });
 });
