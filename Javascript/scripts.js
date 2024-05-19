3cc8dc4c89f2869227391af67baea77257f8a4de
juvisingh <141285456+juvisingh@users.noreply.github.com>
2024-05-19 16:46:10 -0700

    updated

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -20,7 +20,7 @@ $(function() {
         location.reload();
     }   
     function home() {
-        window.location.href = "./index.html";
+        window.location.href = "index.html";
     }
     $("#badge").on("click", function() {
         home();
@@ -48,3 +48,30 @@ $(function() {
         $("#minutes").text(minutes)
     }
 });
+
+
+/* Check the location of each element */
+$('.content').each( function(i){
+  
+    var bottom_of_object= $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).height();
+    
+    if( bottom_of_object > bottom_of_window){
+      $(this).addClass('hidden');
+    }
+  });
+  
+  
+  $(window).scroll( function(){
+      /* Check the location of each element hidden */
+      $('.hidden').each( function(i){
+        
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+          /* If the object is completely visible in the window, fadeIn it */
+          if( bottom_of_window > bottom_of_object ){
+            $(this).animate({'opacity':'1'},700);
+          }
+      });
+  });
